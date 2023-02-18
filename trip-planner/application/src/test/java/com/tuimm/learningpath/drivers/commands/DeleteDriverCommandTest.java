d4a9b0f45c6c32f294cd5360984ffd365e0907bc
daniele.defrancesco <daniele@defrancesco@tui.com>
2023-02-18 14:49:01 +0100

    test: add tests for DeleteDriverCommand

---
+++ trip-planner/application/src/test/java/com/tuimm/learningpath/drivers/commands/DeleteDriverCommandTest.java
@@ -0,0 +1,28 @@
+package com.tuimm.learningpath.drivers.commands;
+
+import com.tuimm.learningpath.drivers.DriversRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.UUID;
+
+import static org.mockito.Mockito.*;
+
+class DeleteDriverCommandTest {
+    private DriversRepository driversRepository;
+    private DeleteDriverCommand deleteDriverCommand;
+
+    @BeforeEach
+    void setUp() {
+        driversRepository = mock(DriversRepository.class);
+        deleteDriverCommand = new DeleteDriverCommand(driversRepository);
+    }
+
+    @Test
+    void handle_shouldInvokeRepository() {
+        UUID id = UUID.fromString("00000000-0000-0000-0000-000000000001");
+        DeleteDriverRequest request = DeleteDriverRequest.fromId(id);
+        deleteDriverCommand.handle(request);
+        verify(driversRepository).delete(id);
+    }
+}
