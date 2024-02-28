61e247e6c4127f2f9d1072f3f3930892a701e430
Daniele De Francesco <daniele.defrancesco@tui.com>
2024-02-28 10:01:35 +0100

    :ambulance: Issue#4 | Fix concurrent update of trip stage drivers

--- trip-planner/domain/src/main/java/com/tuimm/learningpath/mediator/MediatorImpl.java
+++ trip-planner/domain/src/main/java/com/tuimm/learningpath/mediator/MediatorImpl.java
@@ -5,6 +5,8 @@ import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
 
 import java.util.Collection;
+import java.util.Comparator;
+import java.util.List;
 
 @Component
 public class MediatorImpl implements Mediator {
@@ -14,6 +16,9 @@ public class MediatorImpl implements Mediator {
     @Lazy
     @Autowired
     private Collection<EventHandler<?>> eventHandlers;
+    @Lazy
+    @Autowired
+    private Collection<RequestInterceptor<?,?>> requestInterceptors;
 
     public MediatorImpl() {
 
@@ -34,7 +39,17 @@ public class MediatorImpl implements Mediator {
                     throw new UnsupportedOperationException(String.format("No handler found for request of type %s.",
                             request.getClass().getSimpleName()));
                 });
-        return (T2) candidate.handleInternal(request);
+        return sendInternal((RequestHandler<T1, T2>) candidate, request);
+    }
+
+    private <T1 extends Request<T2>, T2> T2 sendInternal(RequestHandler<T1, T2> handler, T1 request) {
+        List<RequestInterceptor<T1,T2>> interceptors = requestInterceptors
+                .stream()
+                .filter(interceptor -> interceptor.getRequestType().equals(request.getClass()))
+                .sorted(Comparator.comparing(RequestInterceptor::getOrder))
+                .map(interceptor -> (RequestInterceptor<T1, T2>) interceptor)
+                .toList();
+        return joinHandlers(interceptors, handler).handle(request);
     }
 
     @Override
@@ -42,5 +57,20 @@ public class MediatorImpl implements Mediator {
         eventHandlers.stream()
                 .filter(handler -> handler.getEventType().equals(event.getClass()))
                 .forEach(handler -> handler.handleInternal(event));
+    }
+
+    private static <T1 extends Request<T2>, T2> NextRequestHandler<T1, T2> joinHandlers(List<RequestInterceptor<T1, T2>> interceptors, RequestHandler<T1, T2> handler) {
+        NextRequestHandler<T1, T2> last = createNextRequestHandlerFromRequestHandler(handler);
+        for (int i = interceptors.size() - 1; i >= 0; i--) {
+            last = createNextRequestHandlerFromInterceptor(interceptors.get(i), last);
+        }
+        return last;
+    }
+
+    private static <T1 extends Request<T2>, T2> NextRequestHandler<T1, T2> createNextRequestHandlerFromInterceptor(RequestInterceptor<T1, T2> interceptor, NextRequestHandler<T1, T2> next) {
+        return request-> interceptor.intercept(request, next);
+    }
+    private static <T1 extends Request<T2>, T2> NextRequestHandler<T1, T2> createNextRequestHandlerFromRequestHandler(RequestHandler<T1, T2> handler) {
+        return request -> (T2) handler.handleInternal(request);
     }
 }
